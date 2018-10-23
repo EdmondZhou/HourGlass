d0f94b834ca2ce5b6b352edb328c18b12f0c2faa
Edmond Zhou <19zhoue90@stu.smuhsd.org>
2018-10-23 09:59:48 -0700

    Hourglass first step finished

---
+++ Hourglass/src/Hourglass.java
@@ -0,0 +1,49 @@
+
+public class Hourglass {
+	public static void main(String[] args) {
+		printBase();
+		printTopHalf();
+		System.out.println("     ||");
+		printBotHalf();
+		printBase();
+	}
+	public static void printStringofChars(String str, int amount) {
+		for (int i = 0; i < amount; i++) {
+			System.out.print(str);
+		}
+	}
+
+	public static void printBase() {
+		String side = "|";
+		System.out.print(side);
+		printStringofChars( "\"" , 10);
+		System.out.print(side);
+		System.out.println("");
+	}	
+	public static void printTopHalf() {
+		String sideL ="\\";
+		String sideR ="/";
+		for(int x = 1; x<=4; x++) {
+			printStringofChars(" ",x);
+			System.out.print(sideL);
+			for(int i = -2*x+10; i >= 0; i -= 8) {
+				printStringofChars(":", i);
+			}
+			System.out.print(sideR);
+			System.out.println("");
+		}
+	}
+	public static void printBotHalf() {
+		String sideL ="/";
+		String sideR ="\\";
+		for(int x = 4; x>=1; x--) {
+			printStringofChars(" ",x);
+			System.out.print(sideL);
+			for(int i = -2*x+10; i >= 0; i -= 8) {
+				printStringofChars(":", i);
+			}
+			System.out.print(sideR);
+			System.out.println("");
+		}
+	}
+}
